9ad42177545d1e51aa705e2b808714d45d64099d
suresh582853 <psuresh433@gmail.com>
2023-02-13 19:33:27 +0530

    SearchName

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Parent from './component/Parent';
+
+const App=()=> {
+      
+    
+  return (
+    <div className="App">
+        <Parent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Parent.js
@@ -0,0 +1,41 @@
+import React, { useState,useEffect} from "react";
+
+const Parent=()=>{
+ 
+   
+     const [name,setName]=useState('');
+     const [data,setData]=useState([]);
+
+     useEffect(()=>{
+           fetch('https://jsonplaceholder.typicode.com/users')
+           .then((res)=>res.json())
+           .then((val)=>setData(val));
+     },[]);
+
+
+     
+   return(
+            <div>
+                   <h3>Enter Your Name</h3>
+                    <div>
+                           <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
+                    </div>
+
+                    <div style={{marginLeft:'250px'}}>
+                           {
+                              data.filter(users=>users.name.toLowerCase().includes(name.toLowerCase())).map((item,index)=>{
+                                  return (
+                                           <div key={index} style={{border:'1px solid black',padding:'10px',margin:'10px',maxWidth:'70%'}}>
+                                                 {item.name}
+                                           </div>
+                                  )
+                              })
+                           }
+                    </div>
+            </div>
+   )
+}
+
+export default Parent;
+
+
